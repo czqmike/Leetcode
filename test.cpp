a6b644f517dc0e6072e4229953050618c51f1c48
czqmike <czqmike@foxmail.com>
2020-02-23 12:42:58 +0800

    modify gitignore

--- test.cpp
+++ test.cpp
@@ -1,23 +1,32 @@
 #include <iostream> 
 #include <fstream>
+#include <vector>
+#include <set>
+#include <map>
 using namespace std;
 
 int main() {
-    ofstream outf("out.txt");
-    ifstream inf("in.txt");
+    vector<int> nums = {9, 1, 7, 9, 7, 9, 7};
 
-    if (!inf) {
-        cerr << "infile error!";
-        return -1;
-    }
-    if (!outf) {
-        cerr << "outfile error!";
-        return -2;
+    map<int, int> m;
+    for (auto num : nums) {
+        auto it = m.find(num);
+        if ( it == m.end()) {
+            m.insert(pair<int, int> (num, 1));
+        } else {
+            it->second += 1; 
+        }
     }
 
-    string word;
-    while (inf >> word) {
-        outf << word << ' ';
+    int ret = 0;
+    for (auto p : m) {
+        // cout << p.first << " " << p.second << endl;
+        if (p.second == 1) {
+            ret = p.first;
+            break;
+        }
     }
+    // cout << "ret = " << ret << endl;
+
     return 0;
 }
